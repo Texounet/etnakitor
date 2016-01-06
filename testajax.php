a8bb055394d7d42183651645c804183610d6f180
Optium <valat_k@etna-alternance.net>
2016-01-06 16:13:23 +0100

    ajax

--- testajax.php
+++ testajax.php
@@ -3,25 +3,26 @@
 <head>
 	<title>Etnakitor</title>
 </head>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <body>
 
 
 
 <div id="ajaxbox"></div>
-<?php echo $json; ?>
+
 <script type="text/javascript" >
-function showquestion(str){
-	 var reponse = $.ajax({
+
+window.onload = function showquestion(str){
+	 jQuery.ajax({
            type: "GET",
            dataType:"json",
-           url: "getuser.php",
-           data: "question="+str,
-           success: parse()
+           url: "/API/question.php",
+           data: "question",
+           success: function (data) { 
+           	console.log('ajax');
+    }
        });
-	 consol.log(str);
-	 alert(str);
 }
 </script>
 
