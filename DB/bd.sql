703cafb85459be33dc0bde1ec9e27e0e9139b665
Teixeira <teixeira.alexandre.dev.info@gmail.com>
2016-01-06 10:30:48 +0100

    misse a jour BD

--- DB/bd.sql
+++ DB/bd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jan 06, 2016 at 10:27 AM
+-- Generation Time: Jan 06, 2016 at 10:30 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -39,6 +39,29 @@ INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `yeux`, `promo`, `se
 (1, 'Robin', 'M', 'brun', 'marron', 'prep etna', 'pédago', 'N'),
 (2, 'Stéphanie GUBERN', 'F', 'blonde', 'marron', 'etna', 'pedago', 'N');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `question`) VALUES
+(1, 'question 1'),
+(2, 'question 2'),
+(3, 'question 3'),
+(4, 'question 4'),
+(5, 'question 5'),
+(6, 'question 6');
+
 --
 -- Indexes for dumped tables
 --
@@ -51,6 +74,14 @@ ALTER TABLE `personnes`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_2` (`id`),
+  ADD KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,3 +90,8 @@ ALTER TABLE `personnes`
 --
 ALTER TABLE `personnes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
