3a66ee168c3ea7efe702081e28a27a51683e6316
janvrot <antoine-janvrot@hotmail.fr>
2016-01-07 14:57:35 +0100

    New db + test js

--- DB/Etnakitor.sql
+++ DB/Etnakitor.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Jeu 07 Janvier 2016 à 14:25
+-- Généré le :  Jeu 07 Janvier 2016 à 14:56
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -34,16 +34,17 @@ CREATE TABLE `personnes` (
   `couleur cheveux` varchar(255) NOT NULL,
   `yeux` varchar(20) NOT NULL,
   `service` varchar(20) NOT NULL,
-  `lunette` varchar(2) NOT NULL
+  `lunette` varchar(2) NOT NULL,
+  `promo` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `personnes`
 --
 
-INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur cheveux`, `yeux`, `service`, `lunette`) VALUES
-(1, 'Robin', 'M', 'court', 'brun', 'marron', 'pedago', 'N'),
-(2, 'Stéphanie GUBERN', 'F', 'long', 'blond', 'marron', 'pedago', 'N');
+INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur cheveux`, `yeux`, `service`, `lunette`, `promo`) VALUES
+(1, 'Robin', 'M', 'court', 'brun', 'marron', 'pedago', 'N', 'prep'),
+(2, 'Stéphanie GUBERN', 'F', 'long', 'blond', 'marron', 'pedago', 'N', 'etna');
 
 -- --------------------------------------------------------
 
@@ -54,7 +55,7 @@ INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur cheveux`, `
 CREATE TABLE `question` (
   `id` int(11) NOT NULL,
   `question` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `question`
@@ -77,7 +78,9 @@ INSERT INTO `question` (`id`, `question`) VALUES
 (14, 'Porte elle des lunettes ?'),
 (15, 'Appartient-elle au service pédagogique ?'),
 (16, 'Appartient-elle au service ADM ?'),
-(17, 'Appartient-elle au studio ?');
+(17, 'Appartient-elle au studio ?'),
+(18, 'S''occupe elle des ETNA ?'),
+(19, 'S''occupe elle des PrepEtna ?');
 
 --
 -- Index pour les tables exportées
@@ -109,7 +112,7 @@ ALTER TABLE `personnes`
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
