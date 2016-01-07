ef0db1e883e5df4dea19f8b6acb97587a44120d8
Vanessa Lagardette <vanessalagardette@Vanessas-MacBook-Pro.local>
2016-01-07 11:35:30 +0100

    Front

--- index.php
+++ index.php
@@ -3,12 +3,29 @@
 <head>
   <meta charset="utf-8">
   <title>Etnakitor</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css.css">
   <script src="script.js"></script>
 </head>
 <body>
-  <div>
-  	Test
+  <div id="title">
+  	<h1>Etnakitor</h1>
   </div>
+  <div id="content">
+  	<div id="question">La question sera ecrite ici</div>
+  </div>
+  
+  <div class="reponse">
+  	<div class="wrapper">
+  		<input type="radio" name="choice" id="yes">
+  		<label for="yes">Oui</label>
+  		<input type="radio" name="choice" id="no">
+  		<label for="no">Non</label>
+  		<div class="main">
+  			<div class="inner"></div>
+ 		</div>
+ 	</div>
+ </div>
+
+
 </body>
 </html>
