cefff636105282c858eb578c51174fcf179651f9
Optium <valat_k@etna-alternance.net>
2016-01-08 09:28:31 +0100

    bdd update

--- DB/Etnakitor.sql
+++ DB/Etnakitor.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Jeu 07 Janvier 2016 à 22:15
--- Version du serveur :  5.5.42
--- Version de PHP :  5.6.10
+-- Client :  127.0.0.1
+-- Généré le :  Ven 08 Janvier 2016 à 09:27
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `etnakitor`
@@ -26,23 +26,25 @@ SET time_zone = "+00:00";
 -- Structure de la table `personnes`
 --
 
-CREATE TABLE `personnes` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `personnes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `result` varchar(255) NOT NULL,
   `sexe` varchar(2) NOT NULL,
   `cheveux` varchar(20) NOT NULL,
-  `couleur cheveux` varchar(255) NOT NULL,
+  `couleur_cheveux` varchar(255) NOT NULL,
   `yeux` varchar(20) NOT NULL,
   `service` varchar(20) NOT NULL,
   `lunette` varchar(2) NOT NULL,
-  `promo` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `promo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `personnes`
 --
 
-INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur cheveux`, `yeux`, `service`, `lunette`, `promo`) VALUES
+INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur_cheveux`, `yeux`, `service`, `lunette`, `promo`) VALUES
 (1, 'Robin', 'M', 'court', 'brun', 'marron', 'pedago', 'N', 'prep'),
 (2, 'Stéphanie GUBERN', 'F', 'long', 'blond', 'marron', 'pedago', 'N', 'etna');
 
@@ -52,69 +54,39 @@ INSERT INTO `personnes` (`id`, `result`, `sexe`, `cheveux`, `couleur cheveux`, `
 -- Structure de la table `question`
 --
 
-CREATE TABLE `question` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(255) NOT NULL,
   `reponse` varchar(255) NOT NULL,
-  `group_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+  `group_id` varchar(55) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
 -- Contenu de la table `question`
 --
 
 INSERT INTO `question` (`id`, `question`, `reponse`, `group_id`) VALUES
-(1, 'Est-ce un homme ?', 'M', 1),
-(2, 'Est-ce une femme ?', 'F', 1),
-(3, 'Ses cheveux sont courts ?', 'court', 7),
-(4, 'Ses cheveux sont longs ?', 'long', 7),
-(5, 'Ses cheveux sont blonds ?', 'blond', 2),
-(6, 'Ses cheveux sont bruns ?', 'brun', 2),
-(7, 'Ses cheveux sont roux ?', 'roux', 2),
-(8, 'Ses cheveux sont blancs ?', 'blanc', 2),
-(9, 'Ses cheveux sont gris ?', 'gris', 2),
-(10, 'Ses yeux sont bleus ?', 'bleu', 3),
-(11, 'Ses yeux sont verts ?', 'vert', 3),
-(12, 'Ses yeux sont marrons ?', 'marron', 3),
-(13, 'Ses yeux sont noirs ?', 'noir', 3),
-(14, 'Porte elle des lunettes ?', 'O', 4),
-(15, 'Appartient-elle au service pédagogique ?', 'pedago', 5),
-(16, 'Appartient-elle au service ADM ?', 'ADM', 5),
-(17, 'Appartient-elle au studio ?', 'studio', 5),
-(18, 'S''occupe elle des ETNA ?', 'etna', 6),
-(19, 'S''occupe elle des PrepEtna ?', 'prep', 6);
+(1, 'Est-ce un homme ?', 'M', 'sexe'),
+(2, 'Est-ce une femme ?', 'F', 'sexe'),
+(3, 'Ses cheveux sont courts ?', 'court', 'cheveux'),
+(4, 'Ses cheveux sont longs ?', 'long', 'cheveux'),
+(5, 'Ses cheveux sont blonds ?', 'blond', 'couleur_cheveux'),
+(6, 'Ses cheveux sont bruns ?', 'brun', 'couleur_cheveux'),
+(7, 'Ses cheveux sont roux ?', 'roux', 'couleur_cheveux'),
+(8, 'Ses cheveux sont blancs ?', 'blanc', 'couleur_cheveux'),
+(9, 'Ses cheveux sont gris ?', 'gris', 'couleur_cheveux'),
+(10, 'Ses yeux sont bleus ?', 'bleu', 'yeux'),
+(11, 'Ses yeux sont verts ?', 'vert', 'yeux'),
+(12, 'Ses yeux sont marrons ?', 'marron', 'yeux'),
+(13, 'Ses yeux sont noirs ?', 'noir', 'yeux'),
+(14, 'Porte elle des lunettes ?', 'O', 'lunette'),
+(15, 'Appartient-elle au service pédagogique ?', 'pedago', 'service'),
+(16, 'Appartient-elle au service ADM ?', 'ADM', 'service'),
+(17, 'Appartient-elle au studio ?', 'studio', 'service'),
+(18, 'S''occupe elle des ETNA ?', 'etna', 'promo'),
+(19, 'S''occupe elle des PrepEtna ?', 'prep', 'promo');
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `personnes`
---
-ALTER TABLE `personnes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Index pour la table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `personnes`
---
-ALTER TABLE `personnes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `question`
---
-ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
