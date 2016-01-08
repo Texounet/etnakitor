23a32f8a3ccce85d9b184f1a0b22ea9879d9ad20
Optium <valat_k@etna-alternance.net>
2016-01-08 10:04:02 +0100

    css robotique

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
   <title>Etnakitor</title>
   <link rel="stylesheet" href="css/play.css">
   <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
@@ -13,16 +15,29 @@
     <h1>Etnakitor</h1>
   </div>
 
-<div id="utek">
-  <img src="http://www.clipartlord.com/wp-content/uploads/2014/09/robot29-201x240.png" alt="I'm Utek!">
-</div>
-  
-  <div id="play">
-    <a href="game.php" class="button">
-      <span> Jouer !</span>
-    </a>
-  </div>
+  <div class="row">
+    <div class="container">
+      <div class="col-md-4">
+        <div class="text-center textindex">
+        <h2>Tu la/le trouve joli(e) ? Même lorsqu'il/elle code ? </h2><br>
+        <h2>Le problème ? Tu ne connais pas le nom de cette personne ...</h2><br>
+        <h2>Alors Etnakitor est la solution</h2>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div id="utek">
+          <img src="http://www.clipartlord.com/wp-content/uploads/2014/09/robot29-201x240.png" alt="I'm Utek!">
+        </div>
 
+
+        <div id="play">
+          <a href="game.php" class="button">
+            <span> Jouer !</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
